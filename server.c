38da7d06f8a30e8829aeb13ea5fb9b81779b4455
Elaheh Jabbarifard <elaheh@royal-06.cs.wisc.edu>
2021-04-29 18:00:00 -0500

    thread pool and request buffer

--- server.c
+++ server.c
@@ -15,31 +15,13 @@ struct thread_arg {
     pthread_mutex_t mutex;
     pthread_cond_t not_full;
     pthread_cond_t not_empty;
-    //int is_ready;
-    //int val;
 };
 
 int buffer_size = 0;
 int num_in_buffer = 0;
+int* buffer;
+struct thread_arg lock;
 
-
-/*void *my_thread(void *arg) {
-    struct thread_arg *a = (struct thread_arg *)arg;
-
-    while (1) {
-        pthread_mutex_lock(&a->mutex);
-
-        while (!a->is_ready)
-            pthread_cond_wait(&a->ready, &a->mutex);
-
-        //printf("Got it: %d!\n\tThread id % 10: %d\n", a->val, pthread_self() % 10);
-        a->is_ready = 0;
-
-        pthread_mutex_unlock(&a->mutex);
-    }
-
-    return NULL;
-}*/
 
 // CS537: Parse the new arguments too
 void getargs(int *port, int argc, char *argv[])
@@ -51,35 +33,33 @@ void getargs(int *port, int argc, char *argv[])
   *port = atoi(argv[1]);  
 }
 
-void worker_func(void* args, int *buffer[], int *connfd) {
-  struct thread_arg *a = (struct thread_arg *)arg;
-  //fetch a request from buffer
-  //call requestHandle()
+void worker_func(void* args) {
+  
   requestHandle(connfd);
-  //
-  //assume we already have the scope for everything we need
+  
   while (1) {
 	  //grab the mutex
-	  pthread_mutex_lock(&a->mutex);
-	  if (num_in_buffer == 0) {//if empty
-		  pthread_cond_wait(&a->not_empty, &a->mutex);
+	  pthread_mutex_lock(&lock.mutex);
+	  
+	  while (num_in_buffer == 0) {//if empty
+		  pthread_cond_wait(&lock.not_empty, &lock.mutex);
 	  }
 	  //loop through the buffer to find a slot with a request
-	  else {
-	      for (int i = 0; i < buffer_size; i++) {
-		  if (buffer[i] != 0 && connfd = buffer[i]) {
-			//connfd = buffer[i];
+	  for (int i = 0; i < buffer_size; i++) {
+		  if (buffer[i] != 0) {
+			connfd = buffer[i];
 	  		//mark the request as done
 	  		buffer[i] = 0;
 			num_in_buffer--;
 			//signal the conditional variable the main thread is waiting on
-			pthread_cond_signal(&arg.not_full);
+			pthread_cond_signal(&lock.not_full);
 			break;
 		  }
-	      }
+	  }
 	  //release lock
-	  pthread_mutex_unlock(&a->mutex);
+	  pthread_mutex_unlock(&lock.mutex);
 	  requestHandle(connfd);
+	  Close(connfd);
   }
 }
 
@@ -89,15 +69,15 @@ int main(int argc, char *argv[])
   int listenfd, connfd, port, clientlen;
   struct sockaddr_in clientaddr;
   struct thread_arg *a;
-  //long sleeptime = (long)arg;
+
+  pthread_mutex_init(&lock.mutex, NULL);
+  pthread_mutex_init(&lock.not_full, NULL);
+  pthread_mutex_init(&lock.not_empty, NULL);
 
   getargs(&port, argc, argv);
   int num_threads = atoi(argv[2]);
   int buffer_size = atoi(argv[3]);
-  //buffer: how many request
-  //thread pool : our threads
 
-  //
   // CS537 (Part B): Create & initialize the shared memory region...
   
   // FOR CREATING THE SHM
@@ -125,48 +105,34 @@ int main(int argc, char *argv[])
   //server will allocate the buffer and creates a bunch of worker threads, worker thread will run some worker function
   //
   
-  //int buffer[buffer_size];
-
-  //we need 1 mutex lock (for the buffer) and 2 conditional variables(main thread vs worker threads)
-  //  if the buffer is full, then main thread cannot write a new request to the buffer
-  //  if the buffer is empty, then working threads cannot read any request from the buffer
-  //we need the scope of buffer, the mutex lock and conditional variables
-  //we can either pass them in as parameters, or make them global
   
-  int buffer[buffer_size];
+  buffer = malloc(sizeof(int) * buffer_size);
 
   pthread_t thread_pool[num_threads];
+
   for (int i = 0; i < num_threads; i++) {
-        pthread_create(&thread_pool[i], NULL, void *(*worker_func)(void* args, buffer, connfd), &arg); //*arguments foi worker_func, type void* 
-	
+        pthread_create(&thread_pool[i], NULL, worker_func, NULL);
   }
-  //worker_func is a function pointer, like syscall.c in xv6
 
   listenfd = Open_listenfd(port);
   while (1) {
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-    pthread_mutex_lock(&a->mutex);
+    pthread_mutex_lock(&lock.mutex);
 
-    if (num_in_buffer == buffer_size) {
-            pthread_cond_wait(&a->not_full, &a->mutex);
+    while (num_in_buffer == buffer_size) {
+            pthread_cond_wait(&lock.not_full, &lock.mutex);
     } 
-    else {
-    	    for (int i = 0; i < buffer_size; i++) {
-	        if (buffer[i] == 0) {
+    for (int i = 0; i < buffer_size; i++) {
+        if (buffer[i] == 0) {
 		      buffer[i] = connfd;
 		      num_in_buffer++;
-		      pthread_cond_signal(&arg.not_empty);
+		      pthread_cond_signal(&lock.not_empty);
 		      break;
-	        }
-	    }
+	}
     }
 
     pthread_mutex_unlock(&a->mutex);
-
-    // grab the lock, go through the buffer to find an empty slot, and put connfd to that slot
-    // but if the buffer is full, then the main thread needs to sleep on one conditional variable
-    // when we put the connfd into the buffer, signal on the conditional variable worker threads are waiting on//worker_func is a function pointer, like syscall.c in xv6
 
     // CS537 (Part A): In general, don't handle the request in the main thread.
     // Save the relevant info in a buffer and have one of the worker threads 
